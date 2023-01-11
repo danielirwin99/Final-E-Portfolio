59dd714596e4f75a6052c5dfdc895e774ce8ece3
Daniel <danielirwin99@gmail.com>
2023-01-11 17:56:05 +1100

    Email Js Bugs

---
+++ app.js
@@ -0,0 +1,23 @@
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_0p2se5o",
+      "template_v8ihdjm",
+      event.target,
+      "TDZ4HKfihtlauZ8Er"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact directly on danielirwin99@gmail.com"
+      );
+    });
+}
--- index.js
+++
@@ -1,9 +0,0 @@
-// template_v8ihdjm
-
-// service_0p2se5o
-
-// hNL9Hj0ChSMMoNoiG 
-
-function contact () {
-    console.log('hello')
-}

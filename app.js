814154791f98e14a299f764abf7351543c3f7c6b
Daniel <danielirwin99@gmail.com>
2023-01-12 13:28:22 +1100

    Main Part of Website done

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0
+    const boolInt = isOdd ? -1 : 1
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+  }
+}
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
+
 function contact(event) {
   event.preventDefault();
   const loading = document.querySelector(".modal__overlay--loading");
@@ -6,7 +31,7 @@ function contact(event) {
   emailjs
     .sendForm(
       "service_0p2se5o",
-      "template_v8ihdjm",
+      "template_7ilmd0a",
       event.target,
       "TDZ4HKfihtlauZ8Er"
     )
@@ -17,7 +42,20 @@ function contact(event) {
     .catch(() => {
       loading.classList.remove("modal__overlay--visible");
       alert(
-        "The email service is temporarily unavailable. Please contact directly on danielirwin99@gmail.com"
+        "The email service is temporarily unavaiable. Please contact me directly on danielirwin99@gmail.com."
       );
     });
+
+  loading.classList += " .modal__overlay--visible";
+
+  setTimeout(() => {}, 1000);
+}
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
 }
